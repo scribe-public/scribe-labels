e7690a99b141b253384ae2bac7cc3ff5b4e7aa10
Daniel Nebenzhal from RunPod <dn@scribesecurity.com>
2024-07-11 17:16:51 +0300

    bash -> sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,16 +6,16 @@ json="{"
 # Loop through environment variables starting with GITHUB_
 for var in $(printenv | grep "^GITHUB_" | awk -F= '{print $1}'); do
   # Get the value of the environment variable
-  value=$(printenv $var)
+  value=$(printenv "$var")
   # Escape double quotes in values
-  value=$(echo $value | jq -R .)
+  value=$(echo "$value" | jq -R .)
   # Add the key-value pair to the JSON string
-  json+="\"$var\":$value,"
+  json="${json}\"$var\":$value,"
 done
 
 # Remove the trailing comma and add the closing brace
-json=${json%,}
-json+="}"
+json=$(echo "$json" | sed 's/,$//')
+json="${json}}"
 
 # Set the output
 echo "::set-output name=github_env_json::$json"
